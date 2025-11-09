00af9fbbe36eb33f2f295c2bbc2512192be33731
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-09 21:32:52 +0800

    Executor runAfter ticks task api

---
+++ src/main/java/me/kall/duplicationless/util/Executor.java
@@ -0,0 +1,39 @@
+package me.kall.duplicationless.util;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import me.kall.duplicationless.Duplicationless;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.ServerLifecycleHooks;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = Duplicationless.MOD_ID)
+public class Executor {
+    private static final Int2ObjectMap<List<Runnable>> TASKS = new Int2ObjectOpenHashMap<>();
+
+    public static void runAfter(int ticks, Runnable task) {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        if (server == null) return;
+        int tickCount = server.getTickCount();
+        server.execute(() -> TASKS.computeIfAbsent(tickCount + ticks, key -> new ArrayList<>()).add(task));
+    }
+
+    @SubscribeEvent
+    public static void onServerTick(TickEvent.@NotNull ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            int tick = event.getServer().getTickCount();
+            event.getServer().execute(() -> {
+                List<Runnable> tasks = TASKS.get(tick);
+                if (tasks == null) return;
+                tasks.forEach(Runnable::run);
+                TASKS.remove(tick);
+            });
+        }
+    }
+}
