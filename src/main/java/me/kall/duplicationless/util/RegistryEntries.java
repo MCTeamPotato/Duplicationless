fe12de9b2de9cd1692afd2707608b1a821d61d39
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-22 14:38:37 +0800

    Update RegistryEntries.java

--- src/main/java/me/kall/duplicationless/util/RegistryEntries.java
+++ src/main/java/me/kall/duplicationless/util/RegistryEntries.java
@@ -1,16 +1,11 @@
 package me.kall.duplicationless.util;
 
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.util.Optional;
 
@@ -27,9 +22,7 @@ public class RegistryEntries {
         return Optional.ofNullable(ForgeRegistries.BLOCKS.getValue(id)).orElseThrow(() -> new IllegalStateException("Missing key in Block: " + id));
     }
 
-    public static Holder<Enchantment> enchantment(ResourceKey<Enchantment> id) {
-        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
-        if (server == null) throw new IllegalStateException("Minecraft server is not running, cannot get Enchantment: " + id);
-        return server.registryAccess().registry(Registries.ENCHANTMENT).flatMap(registry -> registry.getHolder(id)).orElseThrow(() -> new IllegalStateException("Missing key in Enchantment: " + id));
+    public static Enchantment enchantment(ResourceLocation id) {
+        return Optional.ofNullable(ForgeRegistries.ENCHANTMENTS.getValue(id)).orElseThrow(() -> new IllegalStateException("Missing key in Enchantment: " + id));
     }
 }
