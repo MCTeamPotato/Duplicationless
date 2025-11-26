36b3fcd2f5b3392d383f8a018f007c34bb971df2
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-26 14:22:12 +0800

    add toChunk & iterateAround helper methods

--- src/main/java/me/kall/duplicationless/util/Positions.java
+++ src/main/java/me/kall/duplicationless/util/Positions.java
@@ -1,8 +1,12 @@
 package me.kall.duplicationless.util;
 
+import it.unimi.dsi.fastutil.longs.LongConsumer;
 import it.unimi.dsi.fastutil.longs.LongIterator;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.ChunkPos;
 import org.jetbrains.annotations.NotNull;
 
 public final class Positions {
@@ -41,5 +45,37 @@ public final class Positions {
         }
 
         return nearest;
+    }
+
+    public static long toChunk(@NotNull BlockPos pos) {
+        return ChunkPos.asLong(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()));
+    }
+
+    public static long toChunk(long blockPos) {
+        return ChunkPos.asLong(SectionPos.blockToSectionCoord(BlockPos.getX(blockPos)), SectionPos.blockToSectionCoord(BlockPos.getZ(blockPos)));
+    }
+
+    public static long toChunk(int blockX, int blockZ) {
+        return ChunkPos.asLong(SectionPos.blockToSectionCoord(blockX), SectionPos.blockToSectionCoord(blockZ));
+    }
+
+    public static long toChunk(double x, double z) {
+        return ChunkPos.asLong(SectionPos.blockToSectionCoord(Mth.floor(x)), SectionPos.blockToSectionCoord(Mth.floor(z)));
+    }
+
+    public static void iterateAround(@NotNull ChunkPos centerChunk, int radius, LongConsumer action) {
+        iterateAround(centerChunk.x, centerChunk.z, radius, action);
+    }
+
+    public static void iterateAround(long centerChunk, int radius, LongConsumer action) {
+        iterateAround(ChunkPos.getX(centerChunk), ChunkPos.getZ(centerChunk), radius, action);
+    }
+
+    public static void iterateAround(int chunkX, int chunkZ, int radius, LongConsumer action) {
+        for (int dx = -radius; dx <= radius; dx++) {
+            for (int dz = -radius; dz <= radius; dz++) {
+                action.accept(ChunkPos.asLong(chunkX + dx, chunkZ + dz));
+            }
+        }
     }
 }
