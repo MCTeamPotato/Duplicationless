6bc81681e216de0f076ea5b873c168dde119f2b5
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-30 17:06:19 +0800

    Update DataRebuilder.java

--- src/main/java/me/kall/duplicationless/ext/DataRebuilder.java
+++ src/main/java/me/kall/duplicationless/ext/DataRebuilder.java
@@ -1,5 +1,8 @@
 package me.kall.duplicationless.ext;
 
+import org.jetbrains.annotations.ApiStatus;
+
+@ApiStatus.Internal
 public interface DataRebuilder {
     boolean duplicationless$rebuilt();
     void duplicationless$setRebuilt();
