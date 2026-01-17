5e0151950d775fe3b2f07b44646d7c22edb33b09
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-01-17 22:47:58 +0800

    1.1.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
-# 1.1.4
-- Add more helper methods to EntityTracker.
+# 1.1.5
+- Abandon 1.18.2 and 1.19.2
+- Port to 1.21.11
+- Update MixinExtras version
--- src/main/java/me/kall/duplicationless/mixin/MixinExtrasInitializer.java
+++ src/main/java/me/kall/duplicationless/mixin/MixinExtrasInitializer.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import java.util.List;
 import java.util.Set;
 
-public class MixinExtrasInit implements IMixinConfigPlugin {
+public class MixinExtrasInitializer implements IMixinConfigPlugin {
     @Override
     public void onLoad(String s) {
         MixinExtrasBootstrap.init();
